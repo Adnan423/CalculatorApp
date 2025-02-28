75dcaf78c427f9ef9866c4f723f3b94eca4e92c2
Adnan Khan <jackmarkiplier@gmail.com>
2025-02-28 18:54:47 -0500

    more code

--- lib/main.dart
+++ lib/main.dart
@@ -26,3 +26,21 @@ class CalculatorHomePage extends StatefulWidget {
   @override
   _CalculatorHomePageState createState() => _CalculatorHomePageState();
 }
+
+class _CalculatorHomePageState extends State<CalculatorHomePage> {
+  String _displayText = '0';    // Text shown on the display
+  String _operator = '';        // +, -, *, /
+  double? _firstOperand;        // Holds the first number
+  double? _secondOperand;       // Holds the second number
+
+  /// Handles number button presses (0-9).
+  void _onNumberPressed(String number) {
+    setState(() {
+      // If display is "0" or after an operation, start fresh
+      if (_displayText == '0') {
+        _displayText = number;
+      } else {
+        _displayText += number;
+      }
+    });
+  }
